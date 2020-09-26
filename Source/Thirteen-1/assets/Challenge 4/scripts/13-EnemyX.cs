21fcdb92fb6e6ec95e841389bbedf4b421a2c31d
Bunedicto <bfili@deakin.edu.au>
2020-09-26 16:41:09 +1000

    Revert from "Testng"

---
+++ Source/Thirteen-1/assets/Challenge 4/scripts/13-EnemyX.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float speedMultiplier = 1.0f;
+    private Rigidbody enemyRb;
+    private GameObject player;
+    private SpawnManagerX spawnScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+
+        spawnScript = GameObject.Find("Spawn").GetComponent<SpawnManagerX>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime * speedMultiplier);
+
+
+        speedMultiplier = spawnScript.waveCount;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+}
